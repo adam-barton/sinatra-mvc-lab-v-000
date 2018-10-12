6dee6e699a3d56d26f86fc151e2c555c2707a4c6
Adam Barton <adam.barton333@gmail.com>
2018-10-12 23:03:16 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -5,40 +5,40 @@ require 'pry'
 class PigLatinizer
 
 
-  # def translate(text)
-  #   # text.to_piglatin
+  def translate(text)
+    # text.to_piglatin
+  end
+  #
+  # def piglatinize(text)
+  #   x = (text.split(" ").length == 1) ? latinize_word(text) : latinize_sentence(text)
+  #   puts x
+  #   x
   # end
-
-  def piglatinize(text)
-    x = (text.split(" ").length == 1) ? latinize_word(text) : latinize_sentence(text)
-    puts x
-    x
-  end
-
-  def consonant?(character)
-    !character.match(/aAeEiIoOuU/)
-  end
-
-  def vowel?(character)
-    character.match(/aAeEiIoOuU/)
-  end
-
-  def latinize_word(word)
-      if word[0].consonant?
-        word  = word + "w"
-        elsif word[0, 1].consonant?
-          word = word.slice(2..-1) + word.slice(0,2)
-        elsif word[0..2].consonant?
-          word = word.slice(3..-1) + word.slice(0,3)
-        else
-          word = word.slice(1..-1) + word.slice(0)
-      end
-      word + "ay"
-    end
-
-  def latinize_sentence(sentence)
-    sentence.split.collect { |word| latinize_word(word) }.join(" ")
-  end
+  #
+  # def consonant?(character)
+  #   !character.match(/aAeEiIoOuU/)
+  # end
+  #
+  # def vowel?(character)
+  #   character.match(/aAeEiIoOuU/)
+  # end
+  #
+  # def latinize_word(word)
+  #     if word[0].consonant?
+  #       word  = word + "w"
+  #       elsif word[0, 1].consonant?
+  #         word = word.slice(2..-1) + word.slice(0,2)
+  #       elsif word[0..2].consonant?
+  #         word = word.slice(3..-1) + word.slice(0,3)
+  #       else
+  #         word = word.slice(1..-1) + word.slice(0)
+  #     end
+  #     word + "ay"
+  #   end
+  #
+  # def latinize_sentence(sentence)
+  #   sentence.split.collect { |word| latinize_word(word) }.join(" ")
+  # end
 
 
 end
