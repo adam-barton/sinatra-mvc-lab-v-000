99a146ebc8abe8062e54cdd7f9de8dfa7d655ee4
Adam Barton <adam.barton333@gmail.com>
2018-10-12 23:12:31 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -5,36 +5,41 @@ require 'pry'
 class PigLatinizer
 
 
-  def piglatinize(text)
+  def translate(text)
      text.to_piglatin
   end
-  #
-  # def piglatinize(text)
-  #   x = (text.split(" ").length == 1) ? latinize_word(text) : latinize_sentence(text)
-  #   puts x
-  #   x
-  # end
-  #
-  # def consonant?(character)
-  #   !character.match(/aAeEiIoOuU/)
-  # end
-  #
-  # def latinize_word(word)
-  #     if word[0].consonant?
-  #       word  = word + "w"
-  #       elsif word[0, 1].consonant?
-  #         word = word.slice(2..-1) + word.slice(0,2)
-  #       elsif word[0..2].consonant?
-  #         word = word.slice(3..-1) + word.slice(0,3)
-  #       else
-  #         word = word.slice(1..-1) + word.slice(0)
-  #     end
-  #     word + "ay"
-  #   end
-  #
-  # def latinize_sentence(sentence)
-  #   sentence.split.collect { |word| latinize_word(word) }.join(" ")
-  # end
 
+    def piglatinize(input_str)
+      x = (input_str.split(" ").length == 1) ? piglatinize_word(input_str) : piglatinize_sentence(input_str)
+      puts x
+      x
+    end
 
-end
+    private
+
+    def consonant?(char)
+      !char.match(/[aAeEiIoOuU]/)
+    end
+
+    def piglatinize_word(word)
+      # word starts with vowel
+      if !consonant?(word[0])
+        word = word + "w"
+      # word starts with 3 consonants
+      elsif consonant?(word[0]) && consonant?(word[1]) && consonant?(word[2])
+        word = word.slice(3..-1) + word.slice(0,3)
+      # word starts with 2 consonants
+      elsif consonant?(word[0]) && consonant?(word[1])
+        word = word.slice(2..-1) + word.slice(0,2)
+      # word starts with 1 consonant
+      else
+        word = word.slice(1..-1) + word.slice(0)
+      end
+      word << "ay"
+    end
+
+    def piglatinize_sentence(sentence)
+      sentence.split.collect { |word| piglatinize_word(word) }.join(" ")
+    end
+
+  end
